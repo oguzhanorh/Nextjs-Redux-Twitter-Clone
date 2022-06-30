b9c19f46ee32b3c911edca38c9925ada388ed670
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-30 22:24:31 +0300

    some-changes

--- nextjs-redux-app/src/pages/api/auth/[...nextauth].js
+++ nextjs-redux-app/src/pages/api/auth/[...nextauth].js
@@ -1,63 +1,64 @@
-import NextAuth from 'next-auth'
+import NextAuth from 'next-auth';
 
-import CrendentialProvider from 'next-auth/providers/credentials'
+import CrendentialProvider from 'next-auth/providers/credentials';
 
 export default NextAuth({
-    providers: [
-        CrendentialProvider({
-            name: 'Credentials',
-            credentials: {
-                username:{label:'username', placeholder:'Kullanıcı adı'},
-                password:{label:'password', placeholder:'Şifre'}
-            },
-            async authorize(credentials){
-                
-                if (credentials.username==='volkan' && credentials.password==='test'){
-                    return {
-                        id: 5,
-                        name: 'Volkan',
-                        surname: 'Şengül',
-                        email: 'iletisim@volkansengul.com',
-                        gender: 'Male',
-                        is_active: true
-                    }
-                } else {
-                    throw Error('Hatalı kullanıcı adı veya şifre')
-                }
-
-            }
-        })
-    ],
-    callbacks: {
-        jwt: async({token, user, account}) => {
-            if (user){
-                const accessToken = '...'
-                token.data = {
-                    id: user.id,
-                    name: user.name,
-                    gender: user.gender
-                }
-            }
-            return token
-        },
-        session: ({session, token}) => {
-            if (token){
-                session.user = token.data
-            }
-            return session
+  providers: [
+    CrendentialProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'username', placeholder: 'Kullanıcı adı' },
+        password: { label: 'password', placeholder: 'Şifre' },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === 'oguz' &&
+          credentials.password === 'test'
+        ) {
+          return {
+            id: 5,
+            name: 'Oguz',
+            surname: 'Orham',
+            email: 'iletisim@oguzorhan.com',
+            gender: 'Male',
+            is_active: true,
+          };
+        } else {
+          throw Error('Hatalı kullanıcı adı veya şifre');
         }
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: async ({ token, user, account }) => {
+      if (user) {
+        const accessToken = '...';
+        token.data = {
+          id: user.id,
+          name: user.name,
+          gender: user.gender,
+        };
+      }
+      return token;
     },
-    session: {
-        jwt: true,
-        maxAge: 60*30,
-        updateAge: 60*5
+    session: ({ session, token }) => {
+      if (token) {
+        session.user = token.data;
+      }
+      return session;
     },
+  },
+  session: {
+    jwt: true,
+    maxAge: 60 * 30,
+    updateAge: 60 * 5,
+  },
+  secret: 'deneme123',
+  jwt: {
     secret: 'deneme123',
-    jwt: {
-        secret: 'deneme123',
-        encryption: true
-    },
-    pages: {
-        signIn: '/auth/login'
-    }
-})
+    encryption: true,
+  },
+  pages: {
+    signIn: '/auth/login',
+  },
+});
--- nextjs-redux-app/src/services/tweetService.js
+++ nextjs-redux-app/src/services/tweetService.js
@@ -1,42 +1,59 @@
 const tweets = [
-    {user:{
-        id:1, name:'volkan', username:'volkan'
-    }, 
-    text:'Merhaba Dünya', 
-    stats:{like:12, comment:3, retweet:5}},
+  {
+    user: {
+      id: 1,
+      name: 'oguz',
+      username: 'oguz',
+    },
+    text: 'Merhaba Dünya',
+    stats: { like: 12, comment: 3, retweet: 5 },
+  },
 
-    {user:{
-        id:1, name:'volkan', username:'volkan'
-    }, 
-    text:'Lorem ipsum', 
-    stats:{like:7, comment:4, retweet:3}},
+  {
+    user: {
+      id: 1,
+      name: 'oguz',
+      username: 'oguz',
+    },
+    text: 'Lorem ipsum',
+    stats: { like: 7, comment: 4, retweet: 3 },
+  },
 
-    {user:{
-        id:1, name:'volkan', username:'volkan'
-    }, 
-    text:'Ragnar was here', 
-    stats:{like:0, comment:0, retweet:0}},
+  {
+    user: {
+      id: 1,
+      name: 'oguz',
+      username: 'oguz',
+    },
+    text: 'Ragnar was here',
+    stats: { like: 0, comment: 0, retweet: 0 },
+  },
 
-    {user:{
-        id:1, name:'volkan', username:'volkan'
-    }, 
-    text:'Deneme 1 2 3 ', 
-    stats:{like:0, comment:0, retweet:1}}
-]
+  {
+    user: {
+      id: 1,
+      name: 'oguz',
+      username: 'oguz',
+    },
+    text: 'Deneme 1 2 3 ',
+    stats: { like: 0, comment: 0, retweet: 1 },
+  },
+];
 
-export async function getTweets(){
-    return await tweets
+export async function getTweets() {
+  return await tweets;
 }
 
-export async function createTweet(text){
-     
-    const payload = {
-        user:{
-            id:1, name:'volkan', username:'volkan'
-        }, 
-        text: text, 
-        stats:{like:0, comment:0, retweet:0}
-    }
-    console.log('-------', payload)
-    return await payload
+export async function createTweet(text) {
+  const payload = {
+    user: {
+      id: 1,
+      name: 'oguz',
+      username: 'oguz',
+    },
+    text: text,
+    stats: { like: 0, comment: 0, retweet: 0 },
+  };
+  console.log('-------', payload);
+  return await payload;
 }
